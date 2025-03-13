ff7bbcb44a2cbb2b070dea357bf4bd9fceac97c0
AfeezTemitope <belloafeez28@gmail.com>
2025-03-14 00:49:41 +0100

    first push

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import {SafeAreaView, View, Text} from "react-native";
+
+const HomeScreen = () => {
+    return(
+        <SafeAreaView>
+            <View>
+                <Text>hi</Text>
+            </View>
+        </SafeAreaView>
+    )
+}
+export default HomeScreen
