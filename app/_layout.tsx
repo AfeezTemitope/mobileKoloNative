f3f179b7f03938dd3e262eb02fc00ec4ff7bcd8f
AfeezTemitope <belloafeez28@gmail.com>
2025-03-14 01:49:07 +0100

    set up the eenvironment for work

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout() {
   return (
     <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
       <Stack>
+        <StatusBar style="auto" />
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
         <Stack.Screen name="+not-found" />
         <Stack.Screen name="index" options={{ headerShown: false}} />
--- app/index.tsx
+++ app/index.tsx
@@ -1,12 +1,53 @@
-import {SafeAreaView, View, Text} from "react-native";
-
+import React from "react";
+import {SafeAreaView, Image, Text, StyleSheet, Platform, TouchableOpacity, Dimensions} from "react-native";
+import {Ionicons} from "@expo/vector-icons";
+const {width} = Dimensions.get("window")
 const HomeScreen = () => {
-    return(
-        <SafeAreaView>
-            <View>
-                <Text>hi</Text>
-            </View>
+    const handleIconPress = () => {
+        console.log('%cHello, styled console!', 'color: blue; font-size: 20px; font-weight: bold; background-color: yellow; padding: 5px; border-radius: 5px;');
+
+    }
+    return (
+        <SafeAreaView style={styles.container}>
+            <Image
+                source={{ uri: "https://plus.unsplash.com/premium_photo-1681589453442-3b383a67fcfb?q=80&w=1588&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" }}
+                style={styles.image}
+            />
+            <Text style={styles.text}>Mobile Savings App</Text>
+            <TouchableOpacity
+                style={styles.iconContainer} onPress={handleIconPress}
+                accessibilityLabel="Planet Icon" accessibilityRole="button">
+                <Ionicons name="planet-outline" size={62} color="#fff" />
+            </TouchableOpacity>
         </SafeAreaView>
-    )
-}
-export default HomeScreen
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Platform.OS === "ios" ? "#f0f0f0" : "#000",
+    },
+    image: {
+        width: "100%",
+        height: "100%",
+        resizeMode: Platform.OS === "ios" ? "cover" : "cover",
+    },
+    iconContainer:{
+        position: "absolute",
+        bottom: 40,
+        alignSelf: "center",
+        padding: 10,
+    },
+    text:{
+      position: "absolute",
+      top: 20,
+      alignSelf: "center",
+      padding: 10,
+      color: "#f0f0f0",
+      fontSize: width * 0.05,
+      fontWeight: "bold"
+    }
+});
+
+export default HomeScreen;
